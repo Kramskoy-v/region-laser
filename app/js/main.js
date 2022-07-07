0e401a2ef1b4bb27c5de21795201afa3e9d330ac
Kramskoy-v <71666173+Kramskoy-v@users.noreply.github.com>
2022-07-07 18:36:44 +0300

    added phpMailer

--- app/js/main.js
+++ app/js/main.js
@@ -31,18 +31,55 @@
 // input type=file
 
 let inputs = document.querySelectorAll('.input__file');
-    Array.prototype.forEach.call(inputs, function (input) {
-      let label = input.nextElementSibling,
-        labelVal = label.querySelector('.input__file-button-text').innerText;
-  
-      input.addEventListener('change', function (e) {
-        let countFiles = '';
-        if (this.files && this.files.length >= 1)
-          countFiles = this.files.length;
-  
-        if (countFiles)
-	      label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
-        else
-          label.querySelector('.input__file-button-text').innerText = labelVal;
-      });
-    });
+Array.prototype.forEach.call(inputs, function (input) {
+	let label = input.nextElementSibling,
+		labelVal = label.querySelector('.input__file-button-text').innerText;
+
+	input.addEventListener('change', function (e) {
+		let countFiles = '';
+		if (this.files && this.files.length >= 1)
+			countFiles = this.files.length;
+
+		if (countFiles)
+			label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+		else
+			label.querySelector('.input__file-button-text').innerText = labelVal;
+	});
+	document.addEventListener('submit', (e) => { 
+		// Очищаем поля формы 
+			e.target.reset(); 
+			label.querySelector('.input__file-button-text').innerText = labelVal;
+		});
+});
+
+
+
+// Отправка данных на сервер
+function send(event, php){
+	console.log("Отправка запроса");
+	event.preventDefault ? event.preventDefault() : event.returnValue = false;
+	var req = new XMLHttpRequest();
+	req.open('POST', php, true);
+	req.onload = function() {
+		if (req.status >= 200 && req.status < 400) {
+		json = JSON.parse(this.response); // Ебанный internet explorer 11
+			console.log(json);
+			
+			// ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+			if (json.result == "success") {
+				
+				// Если сообщение отправлено
+				alert("Сообщение отправлено");
+				
+			} else {
+				// Если произошла ошибка
+				alert("Ошибка. Сообщение не отправлено");
+			}
+		// Если не удалось связаться с php файлом
+		} else {alert("Ошибка сервера. Номер: "+req.status);}}; 
+	
+	// Если не удалось отправить запрос. Стоит блок на хостинге
+	req.onerror = function() {alert("Ошибка отправки запроса");};
+	req.send(new FormData(event.target));
+	}
+	
---
+++ app/pages/send.php
@@ -0,0 +1,86 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$usermail = $_POST['email'];
+$textarea = $_POST['text'];
+$file = $_FILES['myfile'];
+
+
+// Формирование самого письма
+$title = "Заявка с сайта Регион Лазер";
+$body = "
+<h2>Новая заявка с сайта Регион Лазер</h2>
+<b>Имя:</b> $name<br>
+<b>Номер телефона:</b> $phone<br>
+<b>Электронная почта:</b> $usermail<br>
+<b>Комментарий:</b> $textarea<br>";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'kramskoy.va@yandex.ru'; // Логин на почте
+    $mail->Password   = 'oservdzshpxdubty'; // Пароль на почте 42
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('kramskoy.va@yandex.ru', 'Регион Лазер'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+   $mail->addAddress('kramskoy.va@gmail.com');
+
+ // Прикрипление файлов к письму
+ if (!empty($file['name'][0])) {
+    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+        $filename = $file['name'][$ct];
+        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+            $rfile[] = "Файл $filename прикреплён";
+        } else {
+            $rfile[] = "Не удалось прикрепить файл $filename";
+        }
+    }   
+}
+
+// Прикрипление файлов к письму
+if (!empty($file['name'][0])) {
+    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+        $filename = $file['name'][$ct];
+        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+            $rfile[] = "Файл $filename прикреплён";
+        } else {
+            $rfile[] = "Не удалось прикрепить файл $filename";
+        }
+    }   
+}
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
